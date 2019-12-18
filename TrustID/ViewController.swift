cc4eb40a2009a3da8da12da2e77ca0e5bfabf532
Berk Turan <berk.turan2@isik.edu.tr>
2019-12-18 19:37:17 +0300

    Initial Commit

---
+++ TrustID/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrustID
+//
+//  Created by Berk Turan on 12/18/19.
+//  Copyright © 2019 GatePay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
